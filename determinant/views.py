ed74e05a28d7bc86b9596d55a4287875feb92ce3
Sam Ireland <sam.ireland.uk@gmail.com>
2018-11-28 00:45:42 +0000

    Day links

--- determinant/views.py
+++ determinant/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from .models import *
 
 def root(request):
     today = datetime.strptime(request.GET["day"], "%Y-%m-%d").date() if "day" in request.GET else date.today()
     progressium = Progressium(Habit.objects.all(), end=today)
     return render(request, "home.html", {
-     "progressium": progressium
+     "progressium": progressium,
+     "previous": (today - timedelta(days=1)).strftime("%Y-%m-%d"),
+     "next": (today + timedelta(days=1)).strftime("%Y-%m-%d")
     })
